3dea8df618244fb353c1affc65fa7108be0ef2f2
Nico Päller <cayox@outlook.com>
2022-07-06 23:49:25 +0200

    add search box mockup add logo images add custom colors remove grid for car views

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,23 @@ module.exports = {
         '8xl': '96rem',
         '9xl': '128rem',
       },
-    }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'accent': {
+        100: '#EBF7F9',
+        200: '#B8E4E9',
+        300: '#A6DDE3',
+        400: '#95D6DD',
+        500: '#82CED7',
+        600: '#5DC1CC',
+        700: '#2C5F65',
+        800: '#18464B',
+        900: '#061213',
+      },
+      'white': '#ffffff',
+    },
   }
 }
 
